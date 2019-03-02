fdaa6b9f997c6c2db5f95b4a3536bf09164d11f2
Nathan <nathanarohde@gmail.com>
2019-03-01 20:51:56 -0800

    Initial formatting work

--- src/App.js
+++ src/App.js
@@ -5,21 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Dog Finder</h1>
         </header>
+        <div className="row">
+          <nav className="col-sm-3"></nav>
+          <main className="col-sm-9"></main>
+        </div>
       </div>
     );
   }
